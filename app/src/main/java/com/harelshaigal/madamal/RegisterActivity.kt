8d2c728803d601f977346887a1f53e5689d3c1d2
harelush <harelchen1999@gmail.com>
2024-02-16 03:30:07 +0200

    feature: added validation to register

--- app/src/main/java/com/harelshaigal/madamal/RegisterActivity.kt
+++ app/src/main/java/com/harelshaigal/madamal/RegisterActivity.kt
@@ -1,11 +1,46 @@
 package com.harelshaigal.madamal
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import com.harelshaigal.madamal.databinding.ActivityRegisterBinding
+import com.wajahatkarim3.easyvalidation.core.view_ktx.validator
 
 class RegisterActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRegisterBinding;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityRegisterBinding.inflate(layoutInflater);
+        setContentView(binding.root)
+
+        binding.registerLoginButton.setOnClickListener(View.OnClickListener {
+            validateEmailAndPassword()
+        })
+    }
+
+    private fun validateEmailAndPassword() {
+        val isEmailValid = binding.registerEmailEditText.validator()
+            .nonEmpty()
+            .validEmail()
+            .addErrorCallback {
+                binding.registerEmailEditText.error = it
+            }
+            .check()
+
+        val isPasswordValid = binding.registerPasswordEditText.validator()
+            .nonEmpty()
+            .atleastOneNumber()
+            .atleastOneSpecialCharacters()
+            .atleastOneUpperCase()
+            .addErrorCallback {
+                binding.registerPasswordEditText.error = it
+            }
+            .check()
+
+
+        if (isEmailValid && isPasswordValid) {
+            // Both fields are valid, proceed with the next steps
+        }
     }
 }
