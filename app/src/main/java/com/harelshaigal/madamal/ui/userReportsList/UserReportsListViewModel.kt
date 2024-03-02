8a692c2fd4758c36ab748bbd8575872648a1e4f8
galronen10 <galronen10@gmail.com>
2024-03-02 16:21:31 +0200

    change report list name to user reports list

--- app/src/main/java/com/harelshaigal/madamal/ui/userReportsList/UserReportsListViewModel.kt
+++ app/src/main/java/com/harelshaigal/madamal/ui/userReportsList/UserReportsListViewModel.kt
@@ -1,12 +1,12 @@
-package com.harelshaigal.madamal.ui.reportsList
+package com.harelshaigal.madamal.ui.userReportsList
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class ReportsListViewModel : ViewModel() {
+class UserReportsListViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
-        value = "This is report list Fragment"
+        value = "This is user report list Fragment"
     }
     val text: LiveData<String> = _text
 }
