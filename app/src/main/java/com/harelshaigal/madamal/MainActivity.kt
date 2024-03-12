0400e890f43a76b8f2890f6e2b931e8fb6e8b656
galronen10 <galronen10@gmail.com>
2024-03-12 21:36:01 +0200

    pull from dev and start creating map fragment

--- app/src/main/java/com/harelshaigal/madamal/MainActivity.kt
+++ app/src/main/java/com/harelshaigal/madamal/MainActivity.kt
@@ -4,18 +4,11 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.harelshaigal.madamal.databinding.ActivityMainBinding
-import com.google.android.gms.maps.OnMapReadyCallback
 
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
-    private var googleMapRef: GoogleMap? = null
-
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -25,22 +18,11 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val mapFragment = supportFragmentManager
-            .findFragmentById(R.id.mapFragment) as SupportMapFragment
-        mapFragment.getMapAsync(this)
 
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
 
         navView.setupWithNavController(navController)
-    }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        googleMapRef = googleMap
-
-        val location = LatLng(31.97007377827919, 34.772878313889215)
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(location))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12f))
     }
 }
--- app/src/main/java/com/harelshaigal/madamal/ui/reportsMap/ReportsMapFragment.kt
+++ app/src/main/java/com/harelshaigal/madamal/ui/reportsMap/ReportsMapFragment.kt
@@ -7,13 +7,25 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.harelshaigal.madamal.R
 import com.harelshaigal.madamal.databinding.FragmentReportsMapBinding
 
-class ReportsMapFragment : Fragment() {
+class ReportsMapFragment : Fragment(), OnMapReadyCallback {
 
     private lateinit var viewModel: ReportsMapViewModel
 
     private var _binding: FragmentReportsMapBinding? = null
+
+    private lateinit var mapView: MapView
+
+    private var googleMapRef: GoogleMap? = null
+
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -31,16 +43,38 @@ class ReportsMapFragment : Fragment() {
         _binding = FragmentReportsMapBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textReportMap
-        viewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        mapView = binding.mapView
+        mapView.onCreate(savedInstanceState)
+        mapView.getMapAsync(this)
+
         return root
     }
 
+    override fun onResume() {
+        super.onResume()
+        mapView.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView.onLowMemory()
+    }
     override fun onDestroyView() {
         super.onDestroyView()
+        mapView.onDestroy()
         _binding = null
     }
 
+    override fun onMapReady(googleMap: GoogleMap) {
+        googleMapRef = googleMap
+
+        val location = LatLng(31.97007377827919, 34.772878313889215)
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(location))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location, 12f))
+    }
 }
