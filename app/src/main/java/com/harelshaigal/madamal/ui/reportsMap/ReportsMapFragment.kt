574eb87bb188cc4e755e1ba18b5b0d4588c309f0
galronen10 <galronen10@gmail.com>
2024-03-15 19:22:16 +0200

    finish add map

--- app/src/main/java/com/harelshaigal/madamal/ui/reportsMap/ReportsMapFragment.kt
+++ app/src/main/java/com/harelshaigal/madamal/ui/reportsMap/ReportsMapFragment.kt
@@ -4,16 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.MapView
 import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
-import com.harelshaigal.madamal.R
 import com.harelshaigal.madamal.databinding.FragmentReportsMapBinding
 
 class ReportsMapFragment : Fragment(), OnMapReadyCallback {
