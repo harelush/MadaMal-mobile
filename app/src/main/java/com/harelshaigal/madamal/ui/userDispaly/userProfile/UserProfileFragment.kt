ed3d133d37dfc626fec465cd58cb87bf87f84c52
harelush <harelchen1999@gmail.com>
2024-04-01 20:31:39 +0300

    fix: now fetching user data

--- app/src/main/java/com/harelshaigal/madamal/ui/userDispaly/userProfile/UserProfileFragment.kt
+++ app/src/main/java/com/harelshaigal/madamal/ui/userDispaly/userProfile/UserProfileFragment.kt
@@ -1,24 +1,30 @@
 package com.harelshaigal.madamal.ui.userDispaly.userProfile
 
+import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
 import com.harelshaigal.madamal.databinding.FragmentUserProfileBinding
+import com.harelshaigal.madamal.helpers.ImagePickerHelper
+import com.squareup.picasso.Picasso
+import kotlinx.coroutines.tasks.await
 
-class UserProfileFragment : Fragment() {
-
+class UserProfileFragment : Fragment(), ImagePickerHelper.ImagePickerCallback {
     private lateinit var viewModel: UserProfileViewModel
-
     private var _binding: FragmentUserProfileBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
+    private lateinit var imagePickerHelper: ImagePickerHelper
+
+    private var selectedImageUri: Uri? = null
+
+
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -26,27 +32,44 @@ class UserProfileFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-        viewModel =
-            ViewModelProvider(this)[UserProfileViewModel::class.java]
-
+        viewModel = ViewModelProvider(this)[UserProfileViewModel::class.java]
         _binding = FragmentUserProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        binding.editUserButton.setOnClickListener {
-//            EditUserProfileDialogFragment.display(getParentFragmentManager())
-            val db = Firebase.firestore
-            val city = hashMapOf(
-                "name" to "Los Angeles",
-                "state" to "CA",
-                "country" to "USA",
-            )
+        imagePickerHelper = ImagePickerHelper(this, this)
 
-            db.collection("cities").document("LA")
-                .set(city)
-                .addOnSuccessListener { Log.d("harelush", "DocumentSnapshot successfully written!") }
-                .addOnFailureListener { e -> Log.w("harelush", "Error writing document", e) }
+        binding.userProfileProfileImageView.setOnClickListener {
+            imagePickerHelper.openImagePicker()
         }
+
+        val user = Firebase.auth.currentUser
+        val userUID = user?.uid
+
+        // Observe the user LiveData from the ViewModel
+        viewModel.user.observe(viewLifecycleOwner) { user ->
+            // Update UI
+            binding.userProfileEmailText.setText(user?.email ?: "")
+            binding.userProfileFullNameText.setText(user?.fullName ?: "")
+            binding.editUserButton
+        }
+
+        viewModel.imageUrl.observe(viewLifecycleOwner) { imageUrl ->
+            if (imageUrl != null && imageUrl.isNotEmpty()) {
+                Picasso.get().load(imageUrl).into(binding.userProfileProfileImageView)
+            } else {
+                // Can load here deafult image
+            }
+        }
+
+        viewModel.fetchUserData(userUID.toString())
+        viewModel.fetchUserImageUrl(userUID.toString())
+
+        binding.editUserButton.setOnClickListener {
+            selectedImageUri?.let { uri ->
+                viewModel.uploadUserImage(uri)
+            }
+        }
+
         return root
     }
 
@@ -55,4 +78,5 @@ class UserProfileFragment : Fragment() {
         _binding = null
     }
 
+    override fun getImageViewForLoad(): ImageView = binding.userProfileProfileImageView
 }
--- app/src/main/java/com/harelshaigal/madamal/ui/userDispaly/userProfile/UserProfileViewModel.kt
+++ app/src/main/java/com/harelshaigal/madamal/ui/userDispaly/userProfile/UserProfileViewModel.kt
@@ -1,11 +1,66 @@
 package com.harelshaigal.madamal.ui.userDispaly.userProfile
 
+import android.net.Uri
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import com.harelshaigal.madamal.data.User
+import com.google.firebase.storage.FirebaseStorage
+import com.harelshaigal.madamal.helpers.ImagePickerHelper
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class UserProfileViewModel : ViewModel() {
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is user data Fragment"
+    private val db = Firebase.firestore
+
+    private val _user = MutableLiveData<User?>()
+    val user: LiveData<User?> = _user
+
+    private val _imageUrl = MutableLiveData<String>()
+    val imageUrl: LiveData<String> = _imageUrl
+
+    fun fetchUserData(uid: String) {
+        db.collection("users").document(uid).get().addOnSuccessListener { document ->
+            if (document != null) {
+                val user = document.toObject(User::class.java)
+                _user.value = user
+            } else {
+                _user.value = null
+            }
+        }.addOnFailureListener { exception ->
+            _user.value = null
+        }
     }
-    val text: LiveData<String> = _text}
+
+    fun fetchUserImageUrl(uid: String) {
+        val storageRef = FirebaseStorage.getInstance().reference.child("images/$uid/profile.jpg")
+        storageRef.downloadUrl.addOnSuccessListener { uri ->
+            _imageUrl.postValue(uri.toString())
+        }.addOnFailureListener {
+            _imageUrl.postValue("")
+        }
+    }
+
+    fun uploadUserImage(imageUri: Uri) {
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                val userUid = Firebase.auth.currentUser?.uid
+                if (userUid != null) {
+                    val fileName = "images/$userUid/profile.jpg"
+                    val downloadUrl = ImagePickerHelper.uploadImageToFirebaseStorage(
+                        imageUri,
+                        fileName,
+                        null // Context is not used in this example, pass null or adjust accordingly.
+                    )
+                }
+            } catch (e: Exception) {
+                // Handle error
+            }
+        }
+    }
+
+}
